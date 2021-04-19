8e42f299c05dadce32a04916ab7be231783c873d
Clément Lemoine <clementlemoine.cl@gmail.com>
2021-04-19 18:58:59 +0200

    🚧 temp header

---
+++ components/Svgs.vue
@@ -0,0 +1,19 @@
+<template>
+    <svg
+        aria-hidden="true"
+        style="position: absolute; width: 0; height: 0; overflow: hidden"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+    >
+        <defs>
+            <symbol id="icon-arrow" fill="#fff" viewBox="0 0 15 10" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M6.29 1.852V.789l8.42 3.453v1.515L6.29 9.21V8.147l6.499-2.674H.5v-1h12.215L6.289 1.852z"
+                />
+            </symbol>
+        </defs>
+    </svg>
+</template>
