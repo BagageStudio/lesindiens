af85e9a1aa5721d94d5dc104e9200668f92af5a3
menthe <adrien.le.mentheour@gmail.com>
2021-04-19 18:41:31 +0200

    ✨ image module

---
+++ components/ModuleProjectImage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="module-image">
+        <FastImage :image="data.image" full-width />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        data: { type: Object, required: true }
+    },
+    data: () => ({}),
+    computed: {},
+    watch: {},
+    methods: {}
+};
+</script>
+
+<style lang="scss" scoped></style>
