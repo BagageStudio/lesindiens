40f4db1da98ca1e009f284f70026002415682b0b
menthe <adrien.le.mentheour@gmail.com>
2021-04-30 11:43:44 +0200

    👷‍♂️ hero header

--- pages/studio.vue
+++ pages/studio.vue
@@ -3,8 +3,14 @@
         <div class="studio-hero">
             <div class="container">
                 <div class="container-small">
-                    <h1 class="h1 content-pad">{{ studio.title }}</h1>
-                    <div class="subtitle studio-subtitle content-pad" v-html="subtitle" />
+                    <div class="wrapper-cols">
+                        <div class="col-small content-pad">
+                            <h1 class="studio-title">{{ studio.title }}</h1>
+                        </div>
+                        <div class="col-large content-pad">
+                            <div class="studio-subtitle" v-html="subtitle" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -54,11 +60,62 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
 .studio-hero {
     padding: 110px 0 50px;
 }
+.studio-title {
+    font-family: $telegraf;
+    font-weight: 400;
+    font-size: 3.5rem;
+    line-height: 42px;
+    margin: 0;
+}
 .studio-subtitle {
+    font-family: $telegraf;
+    font-weight: 400;
+    font-size: 2.5rem;
+    line-height: 32px;
     margin-top: 20px;
+}
+@media (min-width: $tablet) {
+    .studio-hero {
+        padding: 180px 0 50px;
+    }
+    .studio-title {
+        font-size: 5rem;
+        line-height: 60px;
+    }
+}
+@media (min-width: $desktop-small) {
+    .studio-hero {
+        padding: 245px 0 50px;
+    }
+    .studio-title {
+        font-size: 7.5rem;
+        font-weight: 100;
+        line-height: 55px;
+    }
+    .studio-subtitle {
+        font-size: 3rem;
+        line-height: 37px;
+        margin-top: 0;
+    }
+}
+@media (min-width: $desktop) {
+    .studio-title {
+        font-size: 8.5rem;
+        line-height: 65px;
+    }
+}
+@media (min-width: $desktop-xxl) {
+    .studio-title {
+        font-size: 11rem;
+        line-height: 80px;
+    }
+    .studio-subtitle {
+        font-size: 3.5rem;
+        line-height: 42px;
+    }
 }
 </style>
