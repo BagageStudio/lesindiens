f7264cc3e104038971349b8d79f6103e67775032
menthe <adrien.le.mentheour@gmail.com>
2021-05-25 19:57:33 +0200

    👷‍♂️ page type

---
+++ components/ModuleNumberedList.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="module-numbered-list">
+        <div class="container">
+            <div class="container-small">
+                <div class="wrapper-numered-list">
+                    <div class="wrapper-cols">
+                        <div class="col-small content-pad">
+                            {{ data.title }}
+                        </div>
+                        <div class="col-large content-pad">content</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        data: { type: Object, required: true }
+    },
+    data: () => ({}),
+    computed: {},
+    watch: {},
+    methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper-numered-list {
+    position: relative;
+    padding: 30px 0 50px;
+    &::before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: $gutter;
+        right: $gutter;
+        border-top: 1px solid $grey-5;
+    }
+}
+</style>
---
+++ pages/_slug.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="type">
+        <div class="type-hero">
+            <div class="container">
+                <div class="container-small">
+                    <h1 class="type-title content-pad">{{ page[0].content.title }}</h1>
+                </div>
+            </div>
+        </div>
+        <component :is="module.component" v-for="module in page[0].content.modules" :key="module._uid" :data="module" />
+    </div>
+</template>
+
+<script>
+export default {
+    async asyncData({ app, $config, error, route }) {
+        const page = await app.$storyapi
+            .get('cdn/stories', {
+                version: $config.sBlokVersion,
+                // resolve_relations: 'expertises'
+                by_slugs: route.params.slug
+            })
+            .then(res => {
+                return res.data.stories;
+            })
+            .catch(res => {
+                if (!res.response) {
+                    console.error(res);
+                    error({ statusCode: 404, message: 'Failed to receive content from api' });
+                } else {
+                    console.error(res.response.data);
+                    error({ statusCode: res.response.status, message: res.response.data });
+                }
+            });
+        return { page };
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.type-hero {
+    padding: 110px 0 50px;
+}
+.type-title {
+    font-family: $telegraf;
+    font-size: 6.5rem;
+    line-height: 1;
+    font-weight: 100;
+}
+@media (min-width: $tablet) {
+    .type-hero {
+        padding: 180px 0 50px;
+    }
+    .type-title {
+        font-size: 9rem;
+        line-height: 80px;
+    }
+}
+@media (min-width: $desktop-small) {
+    .type-hero {
+        padding: 245px 0 50px;
+    }
+}
+</style>
