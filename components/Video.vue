79df0daf10517c3fdee00f4917c19d1ceac8f516
menthe <adrien.le.mentheour@gmail.com>
2021-06-11 11:02:12 +0200

    ✨ video module

---
+++ components/Video.vue
@@ -0,0 +1,29 @@
+<template>
+    <video autoplay loop muted playsinline :class="{ 'full-width': fullWidth }">
+        <source :src="video.webm_file.filename" type="video/webm" />
+        <source :src="video.mp4_file.filename" type="video/mp4" />
+    </video>
+</template>
+
+<script>
+export default {
+    props: {
+        video: {
+            type: Object,
+            required: true
+        },
+        fullWidth: {
+            type: Boolean,
+            default: false
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.video {
+    &.full-width {
+        width: 100%;
+    }
+}
+</style>
